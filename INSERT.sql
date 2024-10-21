af27f864b18e5fabf63bdf9d67b8530e1cd24cfc
Mikhail-Belskiy <mikl.belskiy@yandex.ru>
2024-10-21 22:51:46 +0300

    work_on_mistakes_

--- INSERT.sql
+++ INSERT.sql
@@ -1,124 +1,83 @@
 insert into albums (name, years)
-	values ('summer','2005')
-	
-insert into albums (name, years)
-	values ('summer','2006')
-	
-insert into albums (name, years)
-	values ('winter','2004')
-	
-insert into albums (name, years)
-	values ('осень','2006')
-	
-insert into albums (name, years)
-	values ('spring','2005')
-	
-insert into albums (name, years)
-	values ('лето','2019')	
-	
-insert into albums (name, years)
-	values ('зима','2020')	
-	
-insert into albums (name, years)
-	values ('осень','2021')	
+	values 
+		('summer','2005'),
+		('summer','2006'),
+		('winter','2004'),
+		('осень','2006'),
+		('spring','2005'),
+		('лето','2019'),
+		('зима','2020'),
+		('осень','2021'),
+		('разное', '2020');
 	
 insert into collection (name, years)
-	values ('golden pen','2005')
-	
-insert into collection (name, years)
-	values ('микс','2008')
-	
-insert into collection (name, years)
-	values ('childrens','2010')
-	
-insert into collection (name, years)
-	values ('on the road','2006')
-	
-insert into collection (name, years)
-	values ('happy','2006')
-	
-insert into collection (name, years)
-	values ('happy_New','2018')	
-	
-insert into collection (name, years)
-	values ('mix','2022')		
-
-insert into collection (name, years)
-	values ('golden pen','2023')	
+	values 
+		('golden pen','2005'),
+		('микс','2008'),
+		('childrens','2010'),
+		('on the road','2006'),
+		('happy','2006'),
+		('happy_New','2018'),
+		('mix','2022'),
+		('golden pen','2023');
 	
 	
 insert into executor (nickname)
-	values ('ДДТ')
-	
-insert into executor (nickname)
-	values ('Алиса')
-	
-insert into executor (nickname)
-	values ('Баста')
-	
-insert into executor (nickname)
-	values ('Непоседы')
-	
-insert into executor (nickname)
-	values ('Руки вверх!')
-	
-insert into executor (nickname)
-	values ('7Б')
-	
-insert into executor (nickname)
-	values ('Радио тапок')
+	values 
+		('ДДТ'),
+		('Алиса'),
+		('Баста'),
+		('Непоседы'),
+		('Руки вверх!'),
+		('7Б'),
+		('Радио тапок');
 	
 	
 insert into genres ("name")
-	values ('Рок')	
-
-insert into genres ("name")
-	values ('Поп')	
-	
-insert into genres ("name")
-	values ('Детская')	
+	values 
+		('Рок'),
+		('Поп'),
+		('Детская');
 	
 insert into traks (name, duration, album_id)
-	values ('Просвистела', '00:03:42', 1)	
+	values 
+		('Просвистела', '00:03:42', 1),
+		('Это всё...', '00:07:54', 1),
+		('небо словян', '00:04:47', 1),
+		('Сансара', '00:07:54', 2),
+		('Синий трактор', '00:02:14', 3),
+		('Мой путь', '00:04:14', 1),
+		('myself', '00:04:14', 9),
+		('by myself', '00:03:14', 9),
+		('bemy self', '00:03:15', 9),
+		('myself by', '00:03:17', 9),
+		('by myself by',  '00:01:15', 9),
+		('beemy',  '00:00:15', 9),
+		('premyne',  '00:09:15', 9),
+		('oh my god', '00:03:17', 9),
+		('my',  '00:01:25', 9),
+		('own my',  '00:01:16', 9),
+		('my own',  '00:04:45', 9);
 	
-insert into traks (name, duration, album_id)
-	values ('Это всё...', '00:07:54', 1)	
-	
-insert into traks (name, duration, album_id)
-	values ('Просвистела', '00:03:42', 1)
-	
-insert into traks (name, duration, album_id)
-	values ('небо словян', '00:04:47', 1)	
-	
-insert into traks (name, duration, album_id)
-	values ('Сансара', '00:07:54', 2)	
-	
-insert into traks (name, duration, album_id)
-	values ('Синий трактор', '00:02:14', 4)	
-	
-insert into traks (name, duration, album_id)
-	values ('Мой путь', '00:04:14', 1)	
-
-insert into executor_albums (executor , albums)
-	values ('6', '7')	
 	
 insert into executor_albums (executor , albums)
-	values
+	VALUES
+		('6', '7'),
 		('1', '6'),
 		('2', '7'),
 		('3', '8'),
 		('4', '5'),
-		('5', '3')
+		('5', '3');
 		
 insert into executor_genres (executor , genres)
 	values
 		('1', '1'),
 		('2', '1'),
 		('3', '2'),
-		('4', '4'),
+		('4', '3'),
 		('5', '2'),
 		('6', '1'),
-		('7', '1')
+		('7', '1');
 		
 insert into traks_collection (traks_id , collection_id)
 	values
@@ -126,7 +85,7 @@ insert into traks_collection (traks_id , collection_id)
 		('3', '2'),
 		('4', '6'),
 		('5', '8'),
-		('6', '8')
+		('6', '8');
 	
 	
 	
--- select.sql
+++ select.sql
@@ -2,25 +2,28 @@
 select "name", duration 
 from traks
 order by duration desc 
-limit 1
+limit 1;
 
 
 select "name", duration 
 from traks
-where duration > '00:03:30';
+where duration >= '00:03:30';
+
 
 select "name" 
 from collection
 where years >=2018 and years <= 2020
 
+
 select nickname 
 from executor
 where nickname like '% %'
 
+
 select "name" 
 from traks
-where lower (name) like ('%мой%') or "name" like ('%My%')
-
+where lower (name) ~* ' my | my\Z|\Amy |\Amy\Z';
+	
 
 select name, count (nickname)
 from executor_genres eg 
@@ -28,22 +31,35 @@ left join executor e on eg.executor = e.executor_id
 left join genres g on eg.genres = g.genres_id 
 group by name;
 
+
 select count(t.name)
 from traks t 
 join albums a on t.traks_id = a.album_id 
-where years > 2018 and years < 2021
-group by  t.name
+where years > 2018 and years < 2021;
+
 
 select a."name", avg(t.duration) 
 from albums a 
 join traks t on a.album_id = t.traks_id
-group by a."name" 
-
-select nickname, years
-from executor_albums ea 
-left join executor e on ea.executor = e.executor_id 
-left join albums a on ea.albums = a.album_id 
-where years != 2020
+group by a."name";
 
 
+select nickname
+FROM executor
+WHERE nickname NOT in (
+	SELECT nickname
+	from executor_albums ea 
+	left join executor e on ea.executor = e.executor_id 
+	left join albums a on ea.albums = a.album_id 
+	where years = 2020);
+
+
+SELECT DISTINCT c."name" 
+FROM collection c
+JOIN traks_collection tc on c.collection_id = tc.collection_id 
+join traks t on tc.traks_id = t.traks_id 
+join albums a on t.album_id = a.album_id 
+join executor_albums ea on a.album_id = ea.albums 
+join executor e on ea.executor = e.executor_id 
+where nickname = 'Руки вверх!'
 
